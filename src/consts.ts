7e718f91dd417d4302b24b73c8debca2a81c6f2e
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2023-10-05 18:31:12 +0200

    💬 Adding SEO head attributes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "@Llanas - Boris MAURENCE";
+export const SITE_DESCRIPTION =
+  "Boris MAURENCE - Développeur Web Fullstack @Llanas - Portfolio - Perpignan";
