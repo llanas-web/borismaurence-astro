b61065a0d74b7f75e627f3912e2bb958d16ecdc8
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2023-10-21 11:40:15 +0200

    💄 ADding Cocogoose font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Cocogoose", "Atkinson", defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        british: "#506755",
+      },
       animation: {
         flow: "flow 30s linear infinite",
       },
